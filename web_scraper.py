c0b5c359c541bdec08ef9aefb67692a4e41f6a5d
unknown <nzekic@hotmail.com>
2022-04-11 13:02:19 +0200

    refactoring

--- web_scraper.py
+++ web_scraper.py
@@ -5,31 +5,29 @@ from selenium.webdriver.support import expected_conditions as EC # determine whe
 from selenium.common.exceptions import TimeoutException  # handling timeout situation
 import pandas as pd
 
-driver_option = webdriver.ChromeOptions()
-driver_option.add_argument(" — incognito")
+driver_option = webdriver.ChromeOptions() 
+driver_option.add_argument(" — incognito") # start browser in incognito
 
 # Change this to your own chromedriver path!
-chromedriver_path = '/Users/Korisnik/Downloads/Code/chromedriver/chromedriver'
+chromedriver_path = '/Users/...'
 
 def create_webdriver():
     return webdriver.Chrome(executable_path=chromedriver_path, chrome_options=driver_option)
 
 browser = create_webdriver()
-browser.get("") #Page you wish to scrap
-
-i = 0
+browser.get("...") #Page you wish to scrap
 
 # Extract all products
-# "//h1[@class='h3 lh-condensed']"
-products = browser.find_elements(By.XPATH, ("//div[@class='product-right-side clear']"))
+products = browser.find_elements(By.XPATH, ("//div[@class='product-right-side clear']")) # adjust depending on the page
 
-# Extract information for each project
+# Extract information for each product
+i = 0
 product_list = {}
 for prod in products:
 
-    prod_name = prod.find_elements(By.XPATH, ("//h4[@itemprop='name']"))[i].get_attribute("innerHTML")
-    prod_price = prod.find_elements(By.XPATH, ("//span[@class='ppra_price-number snowflake']"))[i].get_attribute("innerHTML")
-    prod_url = prod.find_elements(By.XPATH, ("//a[@class='product-item-name']"))[i].get_attribute('href')
+    prod_name = prod.find_elements(By.XPATH, ("//h4[@itemprop='name']"))[i].get_attribute("innerHTML") # adjust depending on the page
+    prod_price = prod.find_elements(By.XPATH, ("//span[@class='ppra_price-number snowflake']"))[i].get_attribute("innerHTML") # adjust depending on the page
+    prod_url = prod.find_elements(By.XPATH, ("//a[@class='product-item-name']"))[i].get_attribute('href') # adjust depending on the page
 
     product_list[prod_url] = prod_name, prod_price
     i += 1
